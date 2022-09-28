feb4fab6b02fc6f8660df666236c724cd4b5d2e0
Pedro Henrique <105957381+pedrohenriquepd@users.noreply.github.com>
2022-09-28 12:27:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-let btn = document.querySelector(".fa-eye");
-
-btn.addEventListener("click", () => {
-  let inputSenha = document.querySelector("#senha");
-
-  if (inputSenha.getAttribute("type") == "password") {
-    inputSenha.setAttribute("type", "text");
-  } else {
-    inputSenha.setAttribute("type", "password");
-  }
-});
-
 function entrar() {
   let usuario = document.querySelector("#usuario");
-  let userLabel = document.querySelector("#userLabel");
-
   let senha = document.querySelector("#senha");
-  let senhaLabel = document.querySelector("#senhaLabel");
-
+  
+  
   let msgError = document.querySelector("#msgError");
   let listaUser = [];
 
-  let userValid = {
-    nome: "admin",
-    user: "admin",
-    senha: "123456",
-  };
-
-  listaUser = JSON.parse(localStorage.getItem("listaUser"));
-
-  listaUser.forEach((item) => {
-    if (usuario.value == item.userCad && senha.value == item.senhaCad) {
-      userValid = {
-        nome: item.nomeCad,
-        user: item.userCad,
-        senha: item.senhaCad,
-      };
-    }
-  });
-
-  if (usuario.value == userValid.user && senha.value == userValid.senha) {
+  if (usuario.value == 'admin' && senha.value == '123456' ) {
     window.location.href = "https://pedrohenriquepd.github.io/CRUD-Javascript/";
+    
+    /* nao precisa fazer parte de token agora */
 
     let mathRandom = Math.random().toString(16).substr(2);
     let token = mathRandom + mathRandom;
